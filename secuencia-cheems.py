fd4af5840c8ce108c73e665dfc535d01f7e5a632
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-04-16 20:31:35 -0600

    add translation spanish to cheems

--- secuencia-cheems.py
+++ secuencia-cheems.py
@@ -1,4 +1,19 @@
 #1. convertimos en cheems y hallamos n
+#recibir una entrada
+n = input()
+
+#separamos n por palabras
+n = n.split()
+print(n)
+#convertir en cheems, si la palabra tiene r colocar antes m de la r en la palabra, si no tiene r colocar m al final de la palabra
+cheems = []
+for i in n:
+    if 'r' in i:
+        i = i.replace('r', 'mr')
+    else:
+        i = i + 'm'
+    cheems.append(i)
+print(cheems)
 #2.  aplicamos la secuencia
 #3. aplicamos los grupos
 #4. convertimos en octal
