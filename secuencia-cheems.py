c6bf1af651b458bc2a58c6ed84cb220d6dfa7e7c
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-04-16 21:39:12 -0600

    add step 3th and 4th

--- secuencia-cheems.py
+++ secuencia-cheems.py
@@ -28,11 +28,38 @@ for i in cheems:
     secuencias.append(secuencia(i))
 print(secuencias)
 
-
-
 #3. aplicamos los grupos
+newWords = []
+for word in cheems:
+    #separar la palabra por la cantidad de letras de secuencia[i][j]
+    grupo = []
+    newWord = []
+    sumatoria = 0
+    for j in secuencias[cheems.index(word)]:
+        #separar cada word en rangos de 0 a j1 y j1 a j2 y j2 a jn hasta que se acaben las letras
+        if sumatoria < len(word):
+            grupo.append(word[sumatoria:sumatoria+j])
+        else:
+            break
+        sumatoria += j
+    
+    for j in secuencias[cheems.index(word)]:
+        #indice j en secuencias[cheems.index(word)]
+        indice = secuencias[cheems.index(word)].index(j)
+        newWord.append(chr(ord(word[indice]) + j))
+
+    #convertir newWord en string
+    newWord = ''.join(newWord)
+    newWords.append(newWord)
+print(newWords)
+
 #4. convertimos en octal
-#5. remplazamos 0 por 9
+octal = []
+for word in newWords:
+
+    octal.append(''.join(str(oct(ord(i))).lstrip("0o") for i in word))
+print(octal)
+#5. remplazamos 0 por 9 y 7 por 8
 #6. convertimos a letras
 
 
